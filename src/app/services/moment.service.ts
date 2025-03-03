1cc26c3ddaaac40a24a76311470359248bf3f798
Miguel Lima <miguelsousalima@hotmail.com>
2025-03-03 10:22:29 -0300

    Component Messages

--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { Moment } from '../Moment';
-
 import { environment } from 'src/environments/environment';
 
 @Injectable({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
-export const environment = {
+interface Environment {
+  production: boolean;
+  baseApiUrl: string;
+}
+
+export const environment: Environment = {
   production: false,
   baseApiUrl: 'http://localhost:3333/',
 };
